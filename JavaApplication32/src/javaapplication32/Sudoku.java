9ee24b06b4f06832958a0d1b26ae602f176aa662
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-05-25 17:16:26 +0100

    teste

--- JavaApplication32/src/javaapplication32/Sudoku.java
+++ JavaApplication32/src/javaapplication32/Sudoku.java
@@ -18,6 +18,10 @@ public class Sudoku
     int SRN; // square root of N
     int K; // No. Of missing digits
     int matu [][];
+    
+    //teste
+    
+    //teste2
  
     // Constructor
     Sudoku(int N, int K)
