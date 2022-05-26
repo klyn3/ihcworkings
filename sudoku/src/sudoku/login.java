b1494278e937bf11723e03c7e03fad3950774d60
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-05-26 17:47:16 +0100

    login inicio

--- sudoku/src/sudoku/login.java
+++ sudoku/src/sudoku/login.java
@@ -4,7 +4,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 
 /**
  * FXML Controller class
@@ -13,9 +32,19 @@ import javafx.scene.control.TextField;
  */
 public class login implements Initializable {
 
-        @FXML TextField Username; 
-	@FXML TextField Password;
-        @Override
-        public void initialize(URL arg0, ResourceBundle arg1) {
+    @FXML
+    public TextField usernameTextField;
+    @FXML
+    public TextField passwordTextField;
+    @FXML
+    public TextField errorField;
+    @FXML
+    private PasswordField hiddenPasswordTextField;
+    @FXML
+    private CheckBox showPassword;
+        
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
             
-        }}
+    }
+}
