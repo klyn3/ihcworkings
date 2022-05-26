61d8398a21a4a71b13f68790a230c023d9d2c16b
klyn3 <69215487+klyn3@users.noreply.github.com>
2022-05-26 01:32:25 +0100

    Add files via upload

---
+++ sudoku/src/sudoku/Main.java
@@ -0,0 +1,31 @@
+package sudoku;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jpc
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(login);
+        //scene.getStylesheets().add("/css/css.css");
+        stage.setTitle("login");
+        stage.setScene(scene);
+        stage.show();
+    }
+    /**
+     * @param args designed without arguments.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ sudoku/src/sudoku/login.java
@@ -0,0 +1,21 @@
+package sudoku;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author claudior
+ */
+public class login implements Initializable {
+
+        @FXML TextField Username; 
+	@FXML TextField Password;
+        @Override
+        public void initialize(URL arg0, ResourceBundle arg1) {
+            
+        }}
