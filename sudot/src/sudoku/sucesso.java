31b6b28d8b94fc234776c6adad394387a369a681
klyn3 <claudioredondo.01@gmail.com>
2022-06-01 00:48:20 +0100

    .

---
+++ sudot/src/sudoku/sucesso.java
@@ -0,0 +1,55 @@
+package sudoku;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+
+public class sucesso implements Initializable {
+    
+    @FXML
+    public Label Sucesso;
+    @FXML
+    public Label Timer;
+    @FXML
+    public Button Menu;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }  
+    
+    public void buttonMenuPressed() throws Exception {
+        try{
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Niveiscene.fxml"));
+                Parent root = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.initModality(Modality.NONE);
+                stage.setTitle("Dificuldade");
+                stage.setScene(new Scene(root));
+                stage.show();
+                
+                Stage thisStage = (Stage) Sucesso.getScene().getWindow();
+                thisStage.close();
+                thisStage = null; //libertar memória
+            } 
+            catch (IOException e) {}
+    }
+    
+}
