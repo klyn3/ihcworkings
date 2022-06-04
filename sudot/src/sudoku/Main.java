cd16291cdec2f7fd5e09503133fad76b4a46a949
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-06-04 21:08:00 +0100

    Update Main.java

--- sudot/src/sudoku/Main.java
+++ sudot/src/sudoku/Main.java
@@ -6,21 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author jpc
- */
 public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        //inicio do sudoku na página de login
         Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
         Scene scene = new Scene(login);
         stage.setTitle("login");
         stage.setScene(scene);
         stage.show();
     }
-    /**
+    /*
      * @param args designed without arguments.
      */
     public static void main(String[] args) {
